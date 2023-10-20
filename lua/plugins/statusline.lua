fda69bfa0fa3f8e5c1b786d491e6f08b84d9ca22
Alexander Garcia <31865557+Alexander-Garcia@users.noreply.github.com>
2023-10-20 21:37:08 +0200

    feat: add active lsp name to lualine

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -5,6 +5,28 @@ return {
     local lualine = require("lualine")
     local lazy_status = require("lazy.status")
 
+    -- stolen shamelessly from evil lualine 
+    local lsp_server_name = {
+      function()
+        local msg = 'No Active Lsp'
+        local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+        local clients = vim.lsp.get_active_clients()
+        if next(clients) == nil then
+          return msg
+        end
+        for _, client in ipairs(clients) do
+          local filetypes = client.config.filetypes
+          if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+            return client.name
+          end
+        end
+        return msg
+      end,
+      icon = '  LSP:',
+      color = { fg = '#c678dd', gui = 'bold' },
+      padding = 2,
+    }
+
     lualine.setup({
       options = {
         theme = "catppuccin",
@@ -15,10 +37,12 @@ return {
             lazy_status.updates,
             cond = lazy_status.has_updates,
             color = { fg = "#ff9e64" },
+            padding = 1,
           },
+          lsp_server_name,
           { "encoding" },
           { "fileformat" },
-          { "filetype" },
+          { "filetype", padding = 1 },
         },
       },
     })
