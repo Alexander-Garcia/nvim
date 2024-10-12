d85a21b6eced205a33e698a29e13b80816ba4d81
Alexander <31865557+Alexander-Garcia@users.noreply.github.com>
2024-10-11 19:31:20 -0500

    feat(plugins): autopairs, and todo finder

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,11 +5,14 @@ return {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
+    "folke/todo-comments.nvim",
   },
   config = function()
     local telescope = require("telescope")
 
     telescope.load_extension("fzf")
+    -- for integration with todo-comments
+    vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
   end,
   opts = {
     -- just install ripgrep so telescope will automatically use it and ignore .gitignore dirs
