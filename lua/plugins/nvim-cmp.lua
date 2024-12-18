bb1aa9cd0d8193e8a7d7c40f7189eb2dc1e01278
Alexander <31865557+Alexander-Garcia@users.noreply.github.com>
2024-12-18 15:17:11 -0600

    chore(cmp): attempts to map cmdline failed

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -4,7 +4,7 @@ return {
   dependencies = {
     "hrsh7th/cmp-buffer", -- source for text in buffer
     "hrsh7th/cmp-path", -- source for file system paths
-    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-cmdline", -- cmd line completions
     "L3MON4D3/LuaSnip", -- snippet engine
     "saadparwaiz1/cmp_luasnip", -- for autocompletion
     "rafamadriz/friendly-snippets", -- useful snippets
@@ -13,6 +13,15 @@ return {
   config = function()
     local cmp = require("cmp")
     local luasnip = require("luasnip")
+    local mapTable = {
+      ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+      ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+      ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+      ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    }
 
     cmp.setup({
       enabled = function()
@@ -35,15 +44,7 @@ return {
         end,
       },
 
-      mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-        ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
-      }),
+      mapping = cmp.mapping.preset.insert(mapTable),
       -- sources for autocompletion
       sources = cmp.config.sources({
         { name = "copilot" }, -- integrate copilot into cmp
@@ -53,21 +54,19 @@ return {
         { name = "path" }, -- file system paths
       }),
     })
+
     cmp.setup.cmdline({ "/", "?" }, {
-      mapping = cmp.mapping.preset.cmdline(),
       sources = {
         { name = "buffer" },
       },
     })
 
     cmp.setup.cmdline({ ":" }, {
-      mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
         { name = "path" },
       }, {
         { name = "cmdline" },
       }),
-      mathcing = { disallow_symbol_nonprefix_matching = false },
     })
   end,
 }
