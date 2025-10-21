d595d17dcef1addb38d942722a249e2dcb94c307
Alexander <31865557+Alexander-Garcia@users.noreply.github.com>
2025-10-21 00:24:27 -0500

    refactor: ts-tools

--- lua/plugins/ts-tools.lua
+++ lua/plugins/ts-tools.lua
@@ -1,13 +1,11 @@
 return {
   "pmizio/typescript-tools.nvim",
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  config = function()
-    require("typescript-tools").setup({
-      settings = {
-        tsserver_plugins = {
-          "@styled/typescript-styled-plugin",
-        },
+  opts = {
+    settings = {
+      tsserver_plugins = {
+        "@styled/typescript-styled-plugin",
       },
-    })
-  end,
+    },
+  },
 }
